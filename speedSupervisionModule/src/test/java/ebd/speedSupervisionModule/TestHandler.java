d2945c6cd7df633854b58102af8cbf2ddbfd8ae9
LSF <>
2019-09-13 19:59:22 +0200

    Added Tests

---
+++ speedSupervisionModule/src/test/java/ebd/speedSupervisionModule/TestHandler.java
@@ -0,0 +1,35 @@
+package ebd.speedSupervisionModule;
+
+
+import ebd.globalUtils.events.ExceptionEvent;
+import ebd.globalUtils.events.NormalEvent;
+import ebd.speedSupervisionModule.util.events.SsmReportEvent;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+public class TestHandler {
+
+    private EventBus eventBus;
+
+    public TestHandler(EventBus eventBus){
+        this.eventBus = eventBus;
+        this.eventBus.register(this);
+    }
+
+    @Subscribe
+    public void exception(ExceptionEvent ee){
+        ee.exception.printStackTrace();
+    }
+
+    @Subscribe
+    public void normalEvent(NormalEvent ne){
+        if(!ne.getClass().getSimpleName().contains("SsmReportEvent")){
+            System.out.println(ne.getClass().getSimpleName());
+        }
+    }
+
+    @Subscribe
+    public void report(SsmReportEvent sre){
+        System.out.println("To Fast? " + sre.toFast);
+    }
+}
