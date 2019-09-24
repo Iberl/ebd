fefc23d00727f58f92a000883a5a0ff945a84c4f
Carolin Becker <carolin.becker@stud.tu-darmstadt.de>
2019-09-24 17:54:55 +0200

    logging in files and on console, formatted logs

--- logger/src/main/java/Logging.java
+++ logger/src/main/java/Logging.java
@@ -1,43 +1,97 @@
 import ebd.globalUtils.events.ExceptionEvent;
 import ebd.globalUtils.events.NormalEvent;
+import ebd.globalUtils.events.logger.ToLogEvent;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class Logging{
 
-    private Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private Logger logger;
     private EventBus eventBus;
+    private String logPrefix;
+    static String logDateTime;
+    
+    static {
+        //format of logs is defined in resources/logging.properties
+        System.setProperty("java.util.logging.config.file", "resources/logging.properties");
+        File logDirectory = new File("log/");
+        if(!logDirectory.exists()){
+            logDirectory.mkdir();
+        }
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmmss");
+        logDateTime = LocalDateTime.now().format(dateTimeFormatter);
+    }
 
-    public Logging(EventBus eventBus){
+    Handler fileHandlerAll = new FileHandler("log/" + logDateTime +" AllEventBuses.log");
+
+    /**
+     * initializes a logger and registers it on a given EventBus, output in files and on console
+     * @param eventBus
+     * @param trainID with up to five digits
+     * @throws IOException
+     */
+    public Logging(EventBus eventBus, int trainID) throws IOException {
+        String trainIDwlZeros = String.format("%05d", trainID);
+        logPrefix = "LB" + trainIDwlZeros;
+        logger = Logger.getLogger(logPrefix);
+        logger.addHandler(fileHandlerAll);
+        Handler fileHandler = new FileHandler("log/" + logDateTime + " " + logPrefix + ".log");
+        logger.addHandler(fileHandler);
         this.eventBus = eventBus;
         this.eventBus.register(this);
+
     }
 
     /**
-     * logs an exceptionEvent
+     * initializes a logger and registers it on the global EventBus, output in files and on console
+     * @throws IOException
+     */
+    public Logging() throws IOException {
+        logPrefix = "     GB";
+        logger = Logger.getLogger("GlobalEventBus");
+        logger.addHandler(fileHandlerAll);
+        Handler fileHandler = new FileHandler("log/" + logDateTime + " GB.log");
+        logger.addHandler(fileHandler);
+        eventBus = EventBus.getDefault();
+        eventBus.register(this);
+    }
+
+    /**
+     * log when ExceptionEvent occurred
      * @param exceptionEvent
      */
     @Subscribe
     public void onExceptionEvent(ExceptionEvent exceptionEvent){
-        logger.log(Level.SEVERE, "ExceptionEvent recognized" + System.lineSeparator()
-                + "source: " + exceptionEvent.source + System.lineSeparator()
-                + "targets: " + exceptionEvent.targets + System.lineSeparator()
-                + "cause in: " + exceptionEvent.cause.source + System.lineSeparator()
-                + "exceptionEventTyp: " + exceptionEvent.exceptionEventTyp, exceptionEvent.exception);
+        logger.log(Level.SEVERE, logPrefix + ": " + exceptionEvent.source + ": ExceptionEvent occurred", exceptionEvent.exception);
     }
 
     /**
-     * logs a normalEvent
+     * log when NormalEvent occurred
      * @param normalEvent
      */
     @Subscribe
     public void onNormalEvent(NormalEvent normalEvent){
-        logger.info("NormalEvent recognized" + System.lineSeparator()
-                + "source: " + normalEvent.source + System.lineSeparator()
-                + "targets: " + normalEvent.targets);
+        if(!normalEvent.getClass().getName().equals("ebd.globalUtils.events.logger.ToLogEvent")) {
+            logger.fine(logPrefix + ": " + normalEvent.source + ": " + normalEvent.getClass().getSimpleName() + " occurred");
+        }
+    }
+
+    /**
+     * log when ToLogEvent occurred
+     * @param toLogEvent
+     */
+    @Subscribe
+    public void onToLogEvent(ToLogEvent toLogEvent){
+        logger.info(logPrefix + ": " + toLogEvent.source + ": " + toLogEvent.msg);
     }
 }
--- logger/src/test/java/LoggingTest.java
+++ logger/src/test/java/LoggingTest.java
@@ -1,8 +1,11 @@
 import ebd.globalUtils.events.Event;
 import ebd.globalUtils.events.ExceptionEvent;
 import ebd.globalUtils.events.NormalEvent;
+import ebd.globalUtils.events.logger.ToLogEvent;
+import ebd.globalUtils.events.trainStatusMananger.ClockTickEvent;
 import org.greenrobot.eventbus.EventBus;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,33 +20,34 @@ public class LoggingTest{
     private static List<String> normalTargets;
     private static NormalEvent normalEvent;
 
+    private static List<String> toLogTargets;
+    private static ToLogEvent toLogEvent;
+
+    private static List<String> clockTickTargets;
+    private static ClockTickEvent clockTickEvent;
+
     /**
-     * logs an exceptionEvent and a normalEvent after initializing a new logging object
+     * logs an ExceptionEvent, a general NormalEvent, a ToLogEvent and a ClockTickEvent which are posted on
+     * a local EventBus and a global EventBus
      * @param args
      */
-    public static void main(String[] args){
-        new Logging(EventBus.getDefault());
-        logExceptionEvent();
-        logNormalEvent();
-    }
-
-    /**
-     * sends an exceptionEvent to global event bus
-     */
-    public static void logExceptionEvent(){
+    public static void main(String[] args) throws IOException {
+        EventBus localBus = new EventBus();
+        new Logging(localBus, 4711);
+        new Logging();
+        localBus.post(generateExceptionEvent());
+        localBus.post(generateNormalEvent());
+        localBus.post(generateToLogEvent());
+        localBus.post(generateClockTickEvent());
         EventBus.getDefault().post(generateExceptionEvent());
-    }
-
-    /**
-     * sends an normalEvent to global event bus
-     */
-    public static void logNormalEvent(){
         EventBus.getDefault().post(generateNormalEvent());
+        EventBus.getDefault().post(generateToLogEvent());
+        EventBus.getDefault().post(generateClockTickEvent());
     }
 
     /**
-     * generates an exceptionEvent
-     * @return exceptionEvent
+     * generates an ExceptionEvent
+     * @return an ExceptionEvent
      */
     private static ExceptionEvent generateExceptionEvent(){
         causeTargets = Arrays.asList("causeTarget1", "causeTarget2");
@@ -55,12 +59,32 @@ public class LoggingTest{
     }
 
     /**
-     * generates a normalEvent
-     * @return normalEvent
+     * generates a NormalEvent
+     * @return a NormalEvent
      */
     private static NormalEvent generateNormalEvent(){
         normalTargets = Arrays.asList("normalTarget1", "normalTarget2");
         normalEvent = new NormalEvent("source of NormalEvent", normalTargets);
         return normalEvent;
+    }
+
+    /**
+     * generates a ToLogEvent
+     * @return a ToLogEvent
+     */
+    private static ToLogEvent generateToLogEvent(){
+        toLogTargets = Arrays.asList("logTarget1", "logTarget2");
+        toLogEvent = new ToLogEvent("source of ToLogEvent", toLogTargets, "logmessage");
+        return toLogEvent;
+    }
+
+    /**
+     * generates a ClockTickEvent (a special NormalEvent)
+     * @return a ClockTickEvent
+     */
+    private static ClockTickEvent generateClockTickEvent(){
+        clockTickTargets = Arrays.asList("clockTickTarget1", "clockTickTarget2");
+        clockTickEvent = new ClockTickEvent("source of ClockTickEvent", clockTickTargets);
+        return clockTickEvent;
     }
 }
