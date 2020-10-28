cc7125b8a25d05c8da2e6a59df7b5584672e1fb7
Christopher Bernjus <>
2020-10-28 20:19:06 +0100

    fix: remove unnecessary files

--- misc/plan_pro/modell/signalbegriffe_ril_301/_1_8/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// �nderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2020.01.07 um 02:16:09 PM CET 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.plan-pro.org/modell/Signalbegriffe_Ril_301/1.8.0")
-package modell.signalbegriffe_ril_301._1_8;
