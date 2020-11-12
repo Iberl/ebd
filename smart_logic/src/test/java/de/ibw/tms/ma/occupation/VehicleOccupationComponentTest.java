f4a69280b9e56fba197cb93156df70d0315ce3d1
iberl <iberl@verkehr.tu-darmstadt.de>
2020-11-12 11:22:39 +0100

    add configuration for files of trainside

--- smart_logic/src/test/java/de/ibw/tms/ma/occupation/VehicleOccupationComponentTest.java
+++ smart_logic/src/test/java/de/ibw/tms/ma/occupation/VehicleOccupationComponentTest.java
@@ -7,6 +7,7 @@ import de.ibw.tms.intf.SmartClient;
 import de.ibw.util.DefaultRepo;
 import ebd.core.Core;
 import ebd.core.util.events.LoadEvent;
+import ebd.globalUtils.configHandler.ConfigHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class VehicleOccupationComponentTest {
 
     private SmartLogicClient SC = new SmartLogicClient();
-    private Core C = new Core();
+    private Core C = null;
 
     private class smartLogicThread extends Thread {
         @Override
@@ -26,6 +27,12 @@ class VehicleOccupationComponentTest {
     private class RbcThread extends Thread {
         @Override
         public void run() {
+
+            if (ConfigHandler.getInstance() == null){
+                System.out.println("Config Handler could not be opened");
+                System.exit(0);
+            }
+            C = new Core();
             C.load(new LoadEvent("szenario", "szenario"));
         }
     }
@@ -36,7 +43,7 @@ class VehicleOccupationComponentTest {
         Thread.sleep(3000);
         SC.start();
         Thread.sleep(3000);
-        //new RbcThread().start();
+        new RbcThread().start();
         while(true);
 
 
